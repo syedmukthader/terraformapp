ff11b3b934d327274a750101561aaffa34fd3722
syedmukthader <76219785+syedmukthader@users.noreply.github.com>
2022-11-23 00:33:00 +0000

    Backend Services

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 resource "aws_db_subnet_group" "javaapp-rds-subgrp" {
 
-  name = "main"
+  name       = "main"
   subnet_ids = [module.vpc.private_subnets[0], module.vpc.private_subnets[1], module.vpc.private_subnets[2]]
 
   tags = {
@@ -17,30 +17,30 @@ resource "aws_elasticache_subnet_group" "javaapp-ecache-subgrp" {
 }
 
 resource "aws_db_instance" "javaapp-rds" {
-  allocated_storage = 20
-  instance_class = "db.t2.micro"
-  storage_type = "gp2"
-  engine = "mysql"
-  engine_version = "5.6"
-  parameter_group_name = "default.my.sql5.6"
-  multi_az = "false"
-  publicly_accessible = "false"
-  skip_final_snapshot = true # true if want to delete the snapshot / false for saving  snapshot
-  db_subnet_group_name = aws_db_subnet_group.javaapp-rds-subgrp.name
+  allocated_storage      = 20
+  instance_class         = "db.t2.micro"
+  storage_type           = "gp2"
+  engine                 = "mysql"
+  engine_version         = "5.6"
+  parameter_group_name   = "default.my.sql5.6"
+  multi_az               = "false"
+  publicly_accessible    = "false"
+  skip_final_snapshot    = true # true if want to delete the snapshot / false for saving  snapshot
+  db_subnet_group_name   = aws_db_subnet_group.javaapp-rds-subgrp.name
   vpc_security_group_ids = [aws_security_group.javaapp-backend-sg.id]
-  name = var.dbname
-  username = var.dbusername
-  password = var.dbpass
+  db_name                = var.dbname
+  username               = var.dbusername
+  password               = var.dbpass
 }
 
 resource "aws_elasticache_cluster" "javaapp-cache" {
-  cluster_id = "javaapp-cache"
-  engine = "memcached"
-  node_type = "cache.t2.micro"
+  cluster_id           = "javaapp-cache"
+  engine               = "memcached"
+  node_type            = "cache.t2.micro"
   parameter_group_name = "default.memcached1.5"
-  port = 11211
-  security_group_ids = [aws_security_group.javaapp-backend-sg.id]
-  subnet_group_name = aws_elasticache_subnet_group.javaapp-ecache-subgrp.name
+  port                 = 11211
+  security_group_ids   = [aws_security_group.javaapp-backend-sg.id]
+  subnet_group_name    = aws_elasticache_subnet_group.javaapp-ecache-subgrp.name
 }
 
 resource "aws_mq_broker" "javaapp-rmq" {
@@ -48,10 +48,10 @@ resource "aws_mq_broker" "javaapp-rmq" {
   engine_type        = "ActiveMQ"
   engine_version     = "5.15.0"
   host_instance_type = "mq.t2.micro"
-  security_groups = [aws_security_group.javaapp-backend-sg.id]
+  security_groups    = [aws_security_group.javaapp-backend-sg.id]
 
   user {
-    password = var.rmqpass  #coming from variable file
+    password = var.rmqpass #coming from variable file
     username = var.rmquser
   }
 }
