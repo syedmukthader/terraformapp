1967416da37da800b0c601e40a44ff7dc6f37f4b
syedmukthader <76219785+syedmukthader@users.noreply.github.com>
2022-11-24 13:57:03 +0000

    Updated SG v3

--- Secgrup.tf
+++ Secgrup.tf
@@ -33,7 +33,7 @@ resource "aws_security_group" "javaapp-bastion-sg" {
     from_port   = 22
     protocol    = "tcp"
     to_port     = 22
-    cidr_blocks = [0.0.0.0/0]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
